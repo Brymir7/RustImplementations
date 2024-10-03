89edb246368dee6968b02d89afeeff1c2e26b23f
Brymir7 <Brymir7@gmail.com>
2024-10-03 14:13:19 +0200

    add small testing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+use merge::test_merge_two_lists;
+
 pub mod merge;
 pub mod twosum;
 fn main() {
-    
+    test_merge_two_lists();
 }
--- src/merge.rs
+++ src/merge.rs
@@ -1,3 +1,6 @@
+use crate::merge;
+
+#[derive(PartialEq, Eq, Clone, Debug)]
 pub struct ListNode {
     pub val: i32,
     pub next: Option<Box<ListNode>>,
@@ -8,6 +11,26 @@ impl ListNode {
         ListNode {
             next: None,
             val,
+        }
+    }
+    fn new_chain(values: Vec<i32>) -> Self {
+        let mut prehead: ListNode = ListNode::new(-1);
+        let mut curr_node = &mut prehead;
+        for (i, value) in values.iter().enumerate() {
+            curr_node.val = *value;
+            if i != values.len() - 1 {
+                curr_node.next = Some(Box::new(ListNode::new(-1)));
+                curr_node = curr_node.next.as_mut().unwrap();
+            }
+        }
+        return prehead;
+    }
+    fn print(self) {
+        let mut current = &self;
+        print!("{}", current.val);
+        while let Some(ref node) = current.next {
+            print!("{}", node.val);
+            current = node;
         }
     }
 }
@@ -31,4 +54,17 @@ pub fn merge_two_lists(
     }
     cur_node.next = list1.or(list2);
     prehead.next
+}
+
+pub fn test_merge_two_lists() {
+    let mut chain = ListNode::new_chain(vec![1, 2, 4]);
+    let mut chain2 = ListNode::new_chain(vec![1, 3, 4]);
+    println!("Trying to merge following");
+    chain.clone().print();
+    println!("");
+    chain2.clone().print();
+    println!("");
+    println!("Result");
+    let chain3 = merge_two_lists(Some(Box::new(chain)), Some(Box::new(chain2)));
+    chain3.unwrap().print();
 }
